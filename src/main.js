3233db1d599c2c92a3790f7955f4f4ca4ccfc104
leoliu <leoliuyongfei01@gmail.com>
2020-06-11 19:28:28 +0800

    UPDATE

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+//导入全局样式表
 Vue.config.productionTip = false
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,48 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
-Vue.use(VueRouter)
+const Login = () => import('../views/login/Login');
+const Home = () => import('../views/home/Home');
 
-  const routes = [
+
+Vue.use(VueRouter);
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
+  },
+
+  {
+    path: '/login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    component: Home
   }
-]
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+  mode: 'history'
+});
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  // next（）放行   next（‘login/’） 强制跳转
+  if (to.path === '/login') {
+    // console.log(to.path);
+    return next()
+  }
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token');
+  if (!tokenStr) {
+    return next('/login')
+  }
+  next()
+});
 
 export default router
